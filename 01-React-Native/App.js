0e0a58531fd4d13eaa4540f72bf23141de30007b
swaidertc <54114754+swaidertc@users.noreply.github.com>
2020-03-02 14:16:02 -0500

    Position

--- 01-React-Native/App.js
+++ 01-React-Native/App.js
@@ -1,21 +1,50 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image, Button, Alert} from 'react-native';
 
 class ExerciseReactNativ01 extends Component
 {
+
+  greeting = () => { Alert.alert('Hello this is TalentScout Medellin') }
+
   render()
   {
     return(
       <View style={styles.container}> 
 
-        <View style={styles.headerLeft}>
-        </View>
+        <View style={styles.header}>
 
-        <View style={styles.headerRight}>
+          <View style={styles.headerLeft}>
+
+            <Image source={require('./assets/ctm.jpg')} style={styles.logo}/>
+          </View>
+
+          <View style={styles.headerRight}>
+            <Button title="Login" onPress={this.greeting}/>
+
+          </View>
+
         </View>
 
         <View style={styles.body}>
-        <Text>Hello Word</Text>
+
+        <Text>Hello this is TalentScout Medellin</Text>
+
+        </View>
+
+        <View style={styles.footer}>
+
+            <View style={styles.footerone}>
+            <Text>Welcome</Text>
+            </View>
+
+            <View style={styles.footertwo}>
+            <Text>Welcome</Text>
+            </View>
+
+            <View style={styles.footerthree}>
+              <Image source={require('./assets/ctm.jpg')} style={styles.logo}/>
+            </View>
+
         </View>
 
       </View>    
@@ -27,23 +56,64 @@ const styles = StyleSheet.create(
 {
   container : 
   {
-    flex : 1,
-    flexDirection : 'column'   
+    flex : 4,
+    flexDirection : 'column'  
   },
-  headerLeft :
+  logo :
   {
-    flex : 1,
-    backgroundColor: 'yellow'
+    width : 80,
+    height : 80,
+    borderRadius : 150,
+    resizeMode : 'contain'
+  },
+  header :
+  {
+    flex: 3,
+    flexDirection : 'row',
+    marginTop : 5
   },
   headerRight :
   {
     flex : 1,
-    backgroundColor: 'blue'
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft : 200
+  },
+  headerLeft :
+  {
+    flex : 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   body : 
   {
-    flex : 3,
-    backgroundColor: 'red'
+    flex : 4,
+    justifyContent : 'center',
+    alignItems : 'center'
+  },
+  footer : 
+  {
+    flex : 4,
+    flexDirection : 'row',  
+    margin: 0
+  },
+  footerone : 
+  {
+    flex : 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  footertwo : 
+  {
+    flex : 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  footerthree : 
+  {
+    flex : 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 })
 
