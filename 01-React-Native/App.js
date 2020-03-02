d8bfd2bb5942c060fbb9abaefdf06cf3c0280cb7
swaidertc <54114754+swaidertc@users.noreply.github.com>
2020-03-02 12:19:06 -0500

    Colores

--- 01-React-Native/App.js
+++ 01-React-Native/App.js
@@ -1,19 +1,50 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-    </View>
-  );
+class ExerciseReactNativ01 extends Component
+{
+  render()
+  {
+    return(
+      <View style={styles.container}> 
+
+        <View style={styles.headerLeft}>
+        </View>
+
+        <View style={styles.headerRight}>
+        </View>
+
+        <View style={styles.body}>
+        <Text>Hello Word</Text>
+        </View>
+
+      </View>    
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles = StyleSheet.create(
+{
+  container : 
+  {
+    flex : 1,
+    flexDirection : 'column'   
   },
-});
+  headerLeft :
+  {
+    flex : 1,
+    backgroundColor: 'yellow'
+  },
+  headerRight :
+  {
+    flex : 1,
+    backgroundColor: 'blue'
+  },
+  body : 
+  {
+    flex : 3,
+    backgroundColor: 'red'
+  }
+})
+
+export default ExerciseReactNativ01
